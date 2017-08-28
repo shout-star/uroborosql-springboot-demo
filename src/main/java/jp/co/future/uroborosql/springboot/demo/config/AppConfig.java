1078b1a64c12b62707e0c75ed1f19f8c8c5228e8
KENICHI HOSHI <k.hoshi.ki@future.co.jp>
2017-08-29 01:03:30 +0900

    Add Database auto initialization (#1)
    * [WIP]add db init method
    
    * Change DataSource creation from JNDI.

--- src/main/java/jp/co/future/uroborosql/springboot/demo/config/AppConfig.java
+++ src/main/java/jp/co/future/uroborosql/springboot/demo/config/AppConfig.java
@@ -1,22 +1,20 @@
 package jp.co.future.uroborosql.springboot.demo.config;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.tomcat.util.descriptor.web.ContextResource;
+import jp.co.future.uroborosql.SqlAgent;
+import jp.co.future.uroborosql.config.DefaultSqlConfig;
+import jp.co.future.uroborosql.config.SqlConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
-import org.springframework.jndi.JndiObjectFactoryBean;
 
-import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * Spring configuration
@@ -26,8 +24,8 @@ import javax.sql.DataSource;
 @Configuration
 @PropertySource("classpath:application.yml")
 public class AppConfig {
-    @Value("${spring.datasource.name}")
-    private String name;
+
+    private static final Logger LOG = LoggerFactory.getLogger(AppConfig.class);
 
     @Value("${spring.datasource.url}")
     private String url;
@@ -41,53 +39,41 @@ public class AppConfig {
     @Value("${spring.datasource.driver-class-name}")
     private String driverClassName;
 
-    @Value("${spring.datasource.jndi-name}")
-    private String jndiName;
-
-
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        return new TomcatEmbeddedServletContainerFactory() {
-            @Override
-            protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(final Tomcat tomcat) {
-                tomcat.enableNaming();
-                return super.getTomcatEmbeddedServletContainer(tomcat);
-            }
-
-            @Override
-            protected void postProcessContext(final Context context) {
-                ContextResource resource = new ContextResource();
-                resource.setName(name);
-                resource.setType(DataSource.class.getName());
-                resource.setProperty("factory", "org.apache.tomcat.jdbc.pool.DataSourceFactory");
-                resource.setProperty("driverClassName", driverClassName);
-                resource.setProperty("url", url);
-                resource.setProperty("username", username);
-                resource.setProperty("password", password);
-
-                context.getNamingResources().addResource(resource);
-            }
-        };
-    }
+    @Value("${petclinic.db-auto-init}")
+    private Boolean dbAutoInit;
 
     @Bean
     public EmbeddedServletContainerCustomizer containerCustomizer() {
-
         return (container -> {
             ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/");
             container.addErrorPages(error404Page);
         });
     }
 
-    @Bean(destroyMethod = "")
-    public DataSource jndiDataSource() throws IllegalArgumentException, NamingException {
-        JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
-        bean.setJndiName("java:comp/env/" + jndiName);
-//        System.out.println("java:comp/env/" + jndiName);
-        bean.setProxyInterface(DataSource.class);
-        bean.setLookupOnStartup(false);
-        bean.afterPropertiesSet();
-        return (DataSource) bean.getObject();
+    @Bean
+    public DataSource dataSource() {
+        org.apache.tomcat.jdbc.pool.DataSource ds = new org.apache.tomcat.jdbc.pool.DataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driverClassName);
+        return ds;
     }
 
+    @Bean
+    public String initDatabase(DataSource dataSource) throws SQLException {
+        if (dbAutoInit) {
+            SqlConfig config = DefaultSqlConfig.getConfig(dataSource.getConnection());
+            try (SqlAgent agent = config.createAgent()) {
+                agent.required(() -> {
+                    agent.update("setup/schema").count();
+                    agent.update("setup/data").count();
+                });
+            }
+            LOG.info("Complete database initialization.");
+        } else {
+            LOG.info("Skip database initialization.");
+        }
+        return null;
+    }
 }
